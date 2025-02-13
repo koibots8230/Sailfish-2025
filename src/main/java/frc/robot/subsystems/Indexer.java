f3d6545cc047d7b3188ac65f7c5fc02b9a326a42
Jackson Shartzer <jacksonshartzer@gmail.com>
2025-02-12 20:29:24 -0500

    Added some config and periodic thingys

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -1,10 +1,18 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Volts;
+
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.servohub.ServoHub.ResetMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkClosedLoopController;
 
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -18,22 +26,36 @@ public class Indexer extends SubsystemBase {
     private final SparkMax motor;
     private final RelativeEncoder encoder;
     private final SparkMaxConfig motorConfig;
+    private final SparkClosedLoopController closedLoopController;
 
-
+    final Voltage voltage;
+    final Current current;
+    final AngularVelocity velocity;
+    final AngularVelocity setpoint;
+    
     public Indexer(){
         motor = new SparkMax(IndexerConstants.MOTOR_ID, MotorType.kBrushless);
-        
+            
         motorConfig = new SparkMaxConfig();
         motorConfig.closedLoop.p(IndexerConstants.PID.kp);
         motorConfig.closedLoop.velocityFF(IndexerConstants.FEEDFORWARD.kv);
         motor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        config.ClosedLoop.p(IndexerConstants.PID.kp);
+        config.ClosedLoop.velocityFF(IndexerConstants.FEEDFORWARD.kv);
 
         encoder = motor.getEncoder(); 
-
-    }
+    
+        }
+    @Override
+    public void periodic(){
+        voltage = Voltage.ofBaseUnits(motor.getBusVoltage() * motor.getAppliedOutput(), Volts);
+        current = Current.ofBaseUnits(motor.getOutputCurrent(), Amps);
+        velocity = AngularVelocity.ofBaseUnits(encoder.getVelocity(), RPM);
+        }
     
     private void SpinIndexer(double velocity){
-    
+        
+
     }
 
 
