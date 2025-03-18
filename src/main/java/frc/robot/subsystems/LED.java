4b800803288d6b93ab3aa3c614f9a571d4141b4a
NotABotDaniel <dcgottshall@icloud.com>
2025-03-18 00:04:47 -0400

    final state names and phase enums

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Kilo;
+
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -7,15 +9,28 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LEDConstants;
 
 public class LED extends SubsystemBase {
+  private Phase phase;
+  
   public enum State {
-    teleop,
-    endgame,
+    off,
+    normal,
     auto,
-    p1,
-    p2,
-    p3,
-    off
+    teleop,
+    intake,
+    hasCoral,
+    cDeploying,
+    cDeployed,
+    climbing,
+    tada,
+    xanderMode
     
+  }
+
+  public enum Phase {
+    preGame,
+    auto,
+    teleop,
+  
   }
 
   public SerialPort uart;
@@ -25,30 +40,73 @@ public class LED extends SubsystemBase {
   }
 
   private void setLED(State state) {
-    if (state == State.teleop) {
-      uart.writeString("0");// purple and orange pattern
+    if (state == State.off) {
+      uart.writeString("0");
     }
-    if (state == State.endgame) {
-      uart.writeString("1");// rainbow flash
+    if (state == State.normal) {
+      uart.writeString("1");
     }
     if (state == State.auto) {
-      uart.writeString("2");// rainbow gradient
+      uart.writeString("2");
     }
-    if (state == State.p1) {
-      uart.writeString("3");// purple
+    if (state == State.teleop) {
+      uart.writeString("3");
     }
-    if (state == State.p2) {
-      uart.writeString("4");// teal
+    if (state == State.intake) {
+      uart.writeString("4");
     }
-    if (state == State.p3) {
-      uart.writeString("5");// orange
+    if (state == State.hasCoral) {
+      uart.writeString("5");
     }
-    if (state == State.off) {
-      uart.writeString("5");// all LEDs off
+    if (state == State.cDeploying) {
+      uart.writeString("6");
     }
+    if (state == State.cDeployed) {
+      uart.writeString("7");
+    }
+    if (state == State.climbing) {
+      uart.writeString("8");
+    }
+    if (state == State.tada) {
+      uart.writeString("9");
+    }
+    if (state == State.xanderMode) {
+      uart.writeString("10");
+    }
+  }
+
+  private void resetLEDsToPhase() {
+    if (phase == Phase.preGame) {
+      setLED(State.normal);
+    }
+    if (phase == Phase.auto) {
+      setLED(State.auto);
+    }
+    if (phase == Phase.teleop) {
+      setLED(State.teleop);
+    }
+  }
+
+  public void robotInit() {
+    phase = Phase.preGame;
+    phaseCommand();
+  }
+
+  public void autoInit() {
+    phase = Phase.auto;
+    phaseCommand();
+  }
+
+  public void teleopInit() {
+    phase = Phase.teleop;
+    phaseCommand();
   }
 
   public Command LEDCommand(State state) {
     return Commands.runOnce(() -> this.setLED(state), this);
+  }
+
+  public Command phaseCommand() {
+    return Commands.runOnce(() -> this.resetLEDsToPhase(), this);
   }
 }
