152ab6dbe2591c1c7e461e454272550640803698
JakeStew81 <jakestewart711@icloud.com>
2025-03-21 13:59:52 -0400

    LEDs done!

--- src/main/java/frc/robot/commands/ClimbCommands.java
+++ src/main/java/frc/robot/commands/ClimbCommands.java
@@ -2,22 +2,26 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants.ClimberConstants;
 import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.LED;
 import frc.robot.subsystems.LED.State;
 
 public class ClimbCommands {
-  public static Command prepClimb(Climber climber) {
-    return Commands.parallel(
-      climber.setAngleCommand(ClimberConstants.PREP_POSITION),
-      LEDCommand(State.cDeploying));
+  public static Command prepClimb(Climber climber, LED LED) {
+    return Commands.sequence(
+        climber.setAngleCommand(ClimberConstants.PREP_POSITION),
+        LED.LEDCommand(State.cDeploying),
+        Commands.waitUntil(climber::atSetpoint),
+        LED.LEDCommand(State.cDeployed));
   }
 
-  public static Command climb(Climber climber) {
-    return Commands.parallel(
-      climber.setAngleCommand(ClimberConstants.CLIMB_POSITION),
-      LEDCommand(State.climbing));
+  public static Command climb(Climber climber, LED LED) {
+    return Commands.sequence(
+        climber.setAngleCommand(ClimberConstants.CLIMB_POSITION),
+        LED.LEDCommand(State.climbing),
+        Commands.waitUntil(climber::atSetpoint),
+        LED.LEDCommand(State.tada));
   }
 
   public static Command resetClimber(Climber climber) {
